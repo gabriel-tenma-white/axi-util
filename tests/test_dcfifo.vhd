3a4b7cc785bc1d109dbc75be22e33621d266818a
gabu-chan <OwOwOwOwO123@outlook.com>
2019-08-03 03:43:53 +0000

    tests: dcfifo: test all combinations of input/output flow rate

--- tests/test_dcfifo.vhd
+++ tests/test_dcfifo.vhd
@@ -22,8 +22,36 @@ architecture behaviour of test_dcfifo is
 	signal outData: dataOut_t;
 	signal outClk, outValid, outReady: std_logic := '0';
 	
-	constant inClkHPeriod: time := 20.5 ns;
+	constant inClkHPeriod: time := 1.5 ns;
 	
+	function inputSpeed(I: integer) return integer is
+	begin
+		if I<200 then
+			return 5;
+		elsif I<400 then
+			return 1;
+		elsif I<600 then
+			return 7;
+		elsif I<800 then
+			return 1;
+		else
+			return 1;
+		end if;
+	end function;
+	function outputSpeed(I: integer) return integer is
+	begin
+		if I<200 then
+			return 5;
+		elsif I<400 then
+			return 6;
+		elsif I<600 then
+			return 1;
+		elsif I<800 then
+			return 2;
+		else
+			return 1;
+		end if;
+	end function;
 begin
 	inst: entity dcfifo generic map(width=>16,depthOrder=>6)
 		port map(outClk,inClk,
@@ -39,7 +67,7 @@ begin
 		wait for inClkHPeriod; inClk <= '1'; wait for inClkHPeriod; inClk <= '0';
 		for I in 0 to 500 loop
 			inpValid <= '0';
-			if (I mod 3) = 0 then
+			if (I mod inputSpeed(integer(real(I)*1.5))) = 0 then
 				inpValid <= '1';
 				inData <= data_t(to_unsigned(inpValue,16));
 				if inReady='1' then
@@ -63,8 +91,8 @@ begin
 		wait for 120 ns;
 		wait for 1 ns; outClk <= '1'; wait for 1 ns; outClk <= '0';
 		wait for 1 ns; outClk <= '1'; wait for 1 ns; outClk <= '0';
-		for I2 in 2 to 500 loop
-			if (I2 mod 3) = 0 then
+		for I2 in 2 to 750 loop
+			if ((I2) mod outputSpeed(I2)) = 0 then
 				outReady <= '1';
 				expectData := to_unsigned(expectValue, 16);
 				if outValid='1' then
--- tests/test_dcfifo2.vhd
+++ tests/test_dcfifo2.vhd
@@ -21,9 +21,37 @@ architecture behaviour of test_dcfifo2 is
 	signal inClk, inpValid, inReady: std_logic := '0';
 	signal outData: dataOut_t;
 	signal outClk, outValid, outReady: std_logic := '0';
-	
+
+	function inputSpeed(I: integer) return integer is
+	begin
+		if I<200 then
+			return 5;
+		elsif I<400 then
+			return 1;
+		elsif I<600 then
+			return 7;
+		elsif I<800 then
+			return 1;
+		else
+			return 1;
+		end if;
+	end function;
+	function outputSpeed(I: integer) return integer is
+	begin
+		if I<200 then
+			return 5;
+		elsif I<400 then
+			return 6;
+		elsif I<600 then
+			return 1;
+		elsif I<800 then
+			return 2;
+		else
+			return 1;
+		end if;
+	end function;
 begin
-	inst: entity dcfifo2 generic map(16,32,4)
+	inst: entity dcfifo2 generic map(16,32,5,1)
 		port map(outClk,inClk,
 			outValid, outReady, outData, open,
 			inpValid, inReady, inData, open);
@@ -35,9 +63,9 @@ begin
 	begin
 		wait for 1 ns; inClk <= '1'; wait for 1 ns; inClk <= '0';
 		wait for 1 ns; inClk <= '1'; wait for 1 ns; inClk <= '0';
-		for I in 0 to 500 loop
+		for I in 0 to 1000 loop
 			inpValid <= '0';
-			if (I mod 3) = 0 then
+			if (I mod inputSpeed(I)) = 0 then
 				inpValid <= '1';
 				inData <= data_t(to_unsigned(inpValue,16));
 				if inReady='1' then
@@ -60,8 +88,8 @@ begin
 	begin
 		wait for 1 ns; outClk <= '1'; wait for 1 ns; outClk <= '0';
 		wait for 1 ns; outClk <= '1'; wait for 1 ns; outClk <= '0';
-		for I2 in 2 to 500 loop
-			if (I2 mod 3) = 0 then
+		for I2 in 2 to 1000 loop
+			if (I2 mod outputSpeed(integer(real(I2)*1.5))) = 0 then
 				outReady <= '1';
 				expectData := to_unsigned(expectValue+1, 16) & to_unsigned(expectValue, 16);
 				if outValid='1' then
